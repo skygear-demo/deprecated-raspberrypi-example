f9ed9d92e5392739af1c2cc74c8563d504c8a51e
Rick Mak <rick.mak@gmail.com>
2016-09-14 17:35:19 +0800

    Add Skygear auth function

--- index.js
+++ index.js
@@ -5,14 +5,30 @@ const endPoint = process.env.SKYGEAR_ENDPOINT;
 const apiKey = process.env.SKYGEAR_APIKEY;
 const username = process.env.SKYGEAR_USERNAME;
 const password = process.env.SKYGEAR_PASSWORD;
+const interval = 60;
 
 console.log('Initializing health reporting');
-console.log('Skygear endpointi: ', endPoint);
+console.log('Skygear endpoint: ', endPoint);
 console.log('Skygear username: ', username);
+
+function sendHealth() {
+  console.log('Not implemented');
+}
+
 
 skygear.config({
   apiKey: apiKey,
   endPoint: endPoint
 }).then(() => {
   console.log('Skygear initialized');
+  skygear.signupWithUsername(username, password).then(() => {
+    console.log('Sucessfully signup the devices');
+    setInterval(sendHealth, interval);
+  }, (err) => {
+    console.log('Trying to login');
+    skygear.loginWithUsername(username, password).then(() => {
+      console.log('Sucessfully login');
+      setInterval(sendHealth, interval);
+    });
+  });
 });
