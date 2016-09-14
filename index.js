3196d8b095d35138907bf497bc2e5f25a02aaf66
Rick Mak <rick.mak@gmail.com>
2016-09-14 18:57:27 +0800

    Send health date with cup temp

--- index.js
+++ index.js
@@ -1,18 +1,28 @@
 #!/usr/bin/env node
 
+const exec = require('child_process').exec;
 const skygear = require('skygear');
+
 const endPoint = process.env.SKYGEAR_ENDPOINT;
 const apiKey = process.env.SKYGEAR_APIKEY;
 const username = process.env.SKYGEAR_USERNAME;
 const password = process.env.SKYGEAR_PASSWORD;
-const interval = 60;
+const interval = 60 * 1000;
 
 console.log('Initializing health reporting');
 console.log('Skygear endpoint: ', endPoint);
 console.log('Skygear username: ', username);
 
+const HealthModel = skygear.Record.extend('piHealth');
+
 function sendHealth() {
-  console.log('Not implemented');
+  exec(`vcgencmd measure_temp`, (error, stdout, stderr) => {
+    console.log('Temp', stdout);
+    const h = new HealthModel({
+      cpuTemp: stdout
+    });
+    skygear.privateDB.save(h);
+  });
 }
 
 
@@ -23,11 +33,13 @@ skygear.config({
   console.log('Skygear initialized');
   skygear.signupWithUsername(username, password).then(() => {
     console.log('Sucessfully signup the devices');
+    sendHealth();
     setInterval(sendHealth, interval);
   }, (err) => {
     console.log('Trying to login');
     skygear.loginWithUsername(username, password).then(() => {
       console.log('Sucessfully login');
+      sendHealth();
       setInterval(sendHealth, interval);
     });
   });
